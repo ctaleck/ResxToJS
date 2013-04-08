eacc8db9a095947f828771720c2a738e4cb47c0f
Govind <githubgovin@gmail.com>
2013-04-08 13:14:33 -0700

    updating pretty print option..

--- ResxToJS/Implementation/JsonHelper.cs
+++ ResxToJS/Implementation/JsonHelper.cs
@@ -2,47 +2,82 @@
 
 namespace ResxToJs
 {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
 	public class JsonHelper : IJsonHelper
 	{
-		private const string IndentString = "    ";
+		private const string INDENT_STRING = "    ";
 
-		public string PrettyPrintJson(string jsonString)
+		//// Reference - http://stackoverflow.com/questions/4580397/json-formatter-in-c
+		public string PrettyPrintJson(string str)
 		{
+			var indent = 0;
 			var quoted = false;
 			var sb = new StringBuilder();
-			for (var i = 0; i < jsonString.Length; i++)
+			for (var i = 0; i < str.Length; i++)
 			{
-				var ch = jsonString[i];
-				sb.Append(ch);
+				var ch = str[i];
 				switch (ch)
 				{
 					case '{':
-						sb.Append("\n");
+					case '[':
+						sb.Append(ch);
+						if (!quoted)
+						{
+							sb.AppendLine();
+							Enumerable.Range(0, ++indent).ForEach(item => sb.Append(INDENT_STRING));
+						}
 						break;
-
+					case '}':
+					case ']':
+						if (!quoted)
+						{
+							sb.AppendLine();
+							Enumerable.Range(0, --indent).ForEach(item => sb.Append(INDENT_STRING));
+						}
+						sb.Append(ch);
+						break;
 					case '"':
-						var escaped = false;
+						sb.Append(ch);
+						bool escaped = false;
 						var index = i;
-						while (index > 0 && jsonString[--index] == '\\')
+						while (index > 0 && str[--index] == '\\')
 							escaped = !escaped;
 						if (!escaped)
 							quoted = !quoted;
 						break;
-
-					case ':':
-						sb.Append(" ");
-						break;
-
 					case ',':
+						sb.Append(ch);
 						if (!quoted)
 						{
-							sb.Append("\n");
+							sb.AppendLine();
+							Enumerable.Range(0, indent).ForEach(item => sb.Append(INDENT_STRING));
 						}
+						break;
+					case ':':
+						sb.Append(ch);
+						if (!quoted)
+							sb.Append(" ");
+						break;
+					default:
+						sb.Append(ch);
 						break;
 				}
 			}
-
 			return sb.ToString();
+		}
+	}
+
+	static class Extensions
+	{
+		public static void ForEach<T>(this IEnumerable<T> ie, Action<T> action)
+		{
+			foreach (var i in ie)
+			{
+				action(i);
+			}
 		}
 	}
 }
